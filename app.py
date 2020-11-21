da3457a316dac451efe7658071dde2c61f2f0595
stealthanthrax <sansyrox@gmail.com>
2020-11-21 21:52:44 +0530

    Add some logic for recs

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from pprint import pprint
 from flask_cors import CORS
 from flask import Flask, render_template, request
 from flask import jsonify
 
 import firebase_admin
 from firebase_admin import credentials, firestore
+
+import random
+from pprint import pprint
 
 cred = credentials.Certificate('./ServiceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred)
@@ -14,6 +16,33 @@ db = firestore.client()
 app = Flask(__name__)
 
 CORS(app)
+
+
+def update_record(user_name, mood):
+    doc_ref = db.collection('users').document(user_name)
+    doc_ref.update({'mood': mood})
+
+
+def suggest_opposite(user_name):
+    """ A little broken and untested
+    """
+    reference = {
+        'very_positive': 2,
+        'positive': 1,
+        'negative': -1,
+        'very_negative': -2
+    }
+
+    reverse_reference = {value: key for (key, value) in reference.items()}
+
+    doc_ref = db.collection('users').document(user_name)
+    doc = doc_ref.get()
+    old_mood = reference[doc.to_dict()["mood"]]
+    new_mood = -old_mood
+
+    recs = doc_ref.where('mood', '==', reverse_reference[new_mood])
+    # need to check the kind of data it returns
+    return random.choice(recs) if recs else "@sansyrox"
 
 
 def get_tweets(user_name):
