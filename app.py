cb083f6eedc20513b340a2b6d06ddf216a697e83
stealthanthrax <sansyrox@gmail.com>
2020-11-21 20:22:34 +0530

    Add Two different list of objects

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from flask_cors import CORS
 from flask import Flask, render_template, request
 from flask import jsonify
@@ -15,16 +16,33 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def get_tweets(user_name):
+    self_tweets, other_tweets = [], []
+    doc_ref = db.collection('users').document(user_name)
+    doc = doc_ref.get()
+    dictionary = doc.to_dict()
+    additional_info = dictionary["additionalInfo"]
+    for i in dictionary:
+        if i == "additionalInfo" or i == "userCategory":
+            continue
+
+        dict_object = dictionary[i]
+        if dict_object["is_retweeted"] and dict_object["retweet_author"] != additional_info["screen_name"]:
+            other_tweets.append(dict_object)
+        else:
+            self_tweets.append(dict_object)
+    print(other_tweets)
 
 
 @app.route('/recommend_friends', methods=['GET'])
 def w_youtube():
-    pass
+    return {}
 
 
 @app.route('/seek_motivation', methods=['GET'])
 def recommended_songs():
-    return pass
+    return {}
+
+
+if __name__ == "__main__":
+    get_tweets("_bruhhmoment_")
