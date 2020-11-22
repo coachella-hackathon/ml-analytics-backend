5ae2cdce6462440d055548dcd861d649c25a062f
Guillermo Garcia <ggcobo806@gmail.com>
2020-11-22 04:40:34 +0100

    Retrieve happiest and saddest tweet

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,8 @@
 import json
 import random
 from datetime import datetime
+from typing import Tuple
+
 from classification_model import ClassificationModel
 
 DAYS_COEF = 10
@@ -40,27 +42,51 @@ def update_scoring(tweets: list, emotions: list, owned_by_user: bool):
     """
     owner_coef = OWNER_COEF if owned_by_user else (1 - OWNER_COEF)
     for tweet, tweet_emotions in zip(tweets, emotions):
-        delta = datetime.now() - datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
+        delta = datetime.now() - datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y').replace(tzinfo=None)
         # n_days = days since tweet was posted + 1 (to avoid division by zero)
         n_days = delta.days + 1
         activity = tweet['like_count'] + tweet['retweet_count']
         apply_scoring(tweet_emotions, n_days, activity, owner_coef)
 
 
-def classify(self_tweets: list, other_tweets: list) -> str:
+def get_happiest_and_saddest(emotions: list) -> Tuple[int, int]:
+    """
+    Get happiest and saddest index
+    :param emotions: list of lists containing emotions likelihood
+    :return: happiest_tweet_index, saddest_tweet_index
+    """
+    happiest_item = max(emotions, key=lambda e: e[2])  # Based on joy
+    saddest_item = max(emotions, key=lambda e: e[4])  # Based on sadness
+    return emotions.index(happiest_item), emotions.index(saddest_item)
+
+
+def classify(self_tweets: list, other_tweets: list) -> Tuple[str, dict, dict]:
     """
     Classification function to determine mood status based on
     Twitter activity of a user
     :param self_tweets: tweets owned by current user
     :param other_tweets: tweets not owned by current user
-    :return: mood_status
+    :return: mood_status, happiest_tweet, saddest_tweet
     """
+    # If no tweets provided
+    if not self_tweets and not other_tweets:
+        return "Satisfied", {}, {}
+    # If no self_tweets are provided, there will be no happiest or saddest
+    if not self_tweets:
+        happiest_tweet, saddest_tweet = {}, {}
+
     model = ClassificationModel()
     emotions_transformed = []
     for tweets, owned_by_user in ((self_tweets, True), (other_tweets, False)):
-        emotions = model.predict_emotions(__get_tweet_text(tweets))
-        update_scoring(tweets, emotions, owned_by_user)
-        emotions_transformed += emotions
+        if tweets:
+            emotions = model.predict_emotions(__get_tweet_text(tweets))
+
+            if owned_by_user:
+                happiest_index, saddest_index = get_happiest_and_saddest(emotions)
+                happiest_tweet, saddest_tweet = tweets[happiest_index], tweets[saddest_index]
+
+            update_scoring(tweets, emotions, owned_by_user)
+            emotions_transformed += emotions
     final_scores = list(map(lambda l: sum(l) / len(l),
                             zip(*emotions_transformed)))
     anger, fear, joy, love, sadness, surprise = final_scores
@@ -70,14 +96,16 @@ def classify(self_tweets: list, other_tweets: list) -> str:
     # if mean of negative emotions is greater than positive ones
     if negativity > positivity:
         if anger * 2 > fear + sadness:
-            return "Stressed"
+            mood_status = "Stressed"
         else:
-            return "Depressed"
+            mood_status = "Depressed"
     else:
         if positivity >= POSITIVITY_COEF * negativity:
-            return "Cheerful"
+            mood_status = "Cheerful"
         else:
-            return "Satisfied"
+            mood_status = "Satisfied"
+
+    return mood_status, happiest_tweet, saddest_tweet
 
 
 def get_recommendations(mood_status: str) -> dict:
@@ -92,7 +120,3 @@ def get_recommendations(mood_status: str) -> dict:
             raise ValueError(f"Mood status {mood_status} not supported")
     return {"big_accounts": random.sample(recommendations["big_accounts"], 3),
             "hashtags": random.sample(recommendations["hashtags"], 3)}
-
-
-if __name__ == '__main__':
-    classify(["This looks amazing!", "Im so sad..."], ["This looks amazing!", "Im so sad..."])
