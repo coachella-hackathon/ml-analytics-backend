9708dac3a548e9420ba3e5a937ca89e015db7b02
Ben <chessben88@gmail.com>
2020-11-21 18:10:50 -0800

    Added start - analysis route and additional flask function for updating DB

--- app.py
+++ app.py
@@ -24,6 +24,14 @@ def update_record(user_name, mood):
     doc_ref.update({'mood': mood})
 
 
+def update_tweet_emotion(user_name, happiest_tweet, saddest_tweet):
+    doc_ref = db.collection('users').document(user_name)
+    doc_ref.update({
+        'happiest-tweet': happiest_tweet,
+        'saddest-tweet': saddest_tweet,
+    })
+
+
 def suggest_opposite(user_name):
     """ A little broken and untested
     """
@@ -73,30 +81,38 @@ def get_tweets(user_name):
             other_tweets.append(dict_object)
         else:
             self_tweets.append(dict_object)
-    print(other_tweets)
+    # print(other_tweets)
+    return self_tweets, other_tweets
 
 
-@app.route('/start_analysis/<user_id>', methods=['GET'])
-def start_analysis(user_id):
+@app.route('/start_analysis/<user_name>', methods=['GET'])
+def start_analysis(user_name):
     # username as the parameter
-    # query the db of the username
+
+    # query the db of the username; get tweets.
+    tweets = get_tweets(user_name)
+
     # call the function of ML guys
-    return {}
+    mood_status, happiest_tweet, saddest_tweet = classify(tweets)
+
+    # update DB
+    update_record(user_name, mood_status)
+    update_tweet_emotion(user_name, happiest_tweet, saddest_tweet)
 
 
-@app.route('/recommend_friends/<user_id>', methods=['GET'])
-def w_youtube(user_id):
+@app.route('/recommend_friends/<user_name>', methods=['GET'])
+def w_youtube(user_name):
     # call to Analytics/ML function to serve the data to this endpoint
     return {}
 
 
-@app.route('/seek_motivation/<user_id>', methods=['GET'])
-def recommended_songs(user_id):
+@app.route('/seek_motivation/<user_name>', methods=['GET'])
+def recommended_songs(user_name):
     # call to Analytics/ML function to serve the data to this endpoint
     data = {"object1": {
         "obj2": "hello",
         "obj3": "goodbye",
-        "obj4": user_id
+        "obj4": user_name
     }}
     return jsonify(data)
 
